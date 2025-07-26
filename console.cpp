4c1562313a23215080dbc245e390ae3a3927ccc5
yancey <yancey@whist.com>
2025-07-26 01:06:00 -0400

    support up/down arrow in console

--- console.cpp
+++ console.cpp
@@ -31,7 +31,7 @@ void draw_console(){
 		for(int row=DRAW_HEIGHT_START;row-DRAW_HEIGHT_START<DRAW_HEIGHT;row++){
 			char c=to_draw[i]-FONT_START;
 			lcd_buf[row_size*(row+line* (DRAW_HEIGHT)) +i]= font8x16[c*FONT_HEIGHT+row];
-			if(row-DRAW_HEIGHT_START == DRAW_HEIGHT -1){
+			if(row-DRAW_HEIGHT_START >= DRAW_HEIGHT -2){
 				if(i==cursor+promot.length()){
 					lcd_buf[row_size*(row+line* (DRAW_HEIGHT)) +i]^=0xff; //set the last bit to 1
 				}
@@ -48,6 +48,10 @@ void input_text(string a){
 	}
 }
 
+deque<string> history;
+int history_index=0;
+string save_of_current;
+
 void handle_console(signed int sym, bool key_down){
 	if(!key_down) return;
 	if(!shift_down && sym==SDLK_BACKQUOTE){
@@ -57,6 +61,9 @@ void handle_console(signed int sym, bool key_down){
 		//printf("console %s\n", console_on ? "on" : "off");
 	}
 	if(!console_on) return;
+	if(sym!=SDLK_UP &&sym!=SDLK_DOWN && sym!=SDLK_LEFT && sym!=SDLK_RIGHT){
+		history_index=0;
+	}
 	if(sym== SDLK_BACKSPACE){
 		if(!console_input.empty()) {
 			if(cursor>0){
@@ -68,12 +75,33 @@ void handle_console(signed int sym, bool key_down){
 	if(sym==SDLK_LEFT){
 		cursor--;
 		if(shift_down) cursor-=9;
-		if(cursor<0) cursor=0;
+
 	}
 	if(sym==SDLK_RIGHT){
 		cursor++;
 		if(shift_down) cursor+=9;
-		if(cursor>console_input.length()) cursor=console_input.length();
+	}
+	if(sym==SDLK_UP){
+		if(history_index==0){
+			save_of_current=console_input;
+		}
+		if(history_index<history.size()){
+			history_index++;
+			if(history_index>0){
+				console_input=history[history.size()-history_index];
+			}
+		}
+
+	}
+	if(sym==SDLK_DOWN){
+		if(history_index>0){
+			history_index--;
+			if(history_index>0){
+				console_input=history[history.size()-history_index];
+			}else{
+				console_input=save_of_current;
+			}
+		}
 
 	}
 	if(sym==SDLK_ESCAPE){
@@ -84,11 +112,15 @@ void handle_console(signed int sym, bool key_down){
 	}
 	if(sym==SDLK_RETURN){
 		if(!console_input.empty()){
-			handle_cmd(console_input);
+			push_message(console_input);
+			history.push_back(console_input);
+			if(history.size()>100) history.pop_front();
 			console_input.clear();
 			cursor=0;
 		}
 		console_on=false;
 		SDL_StopTextInput();
 	}
+	if(cursor<0) cursor=0;
+	if(cursor>console_input.length()) cursor=console_input.length();
 }
