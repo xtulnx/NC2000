ab871d79bf5516482ded3e872904cd0a4143836c
yancey <yancey@whist.com>
2025-08-24 18:53:22 -0400

    fix on/off jumping to time; adjust color

--- NekoDriverIO.cpp
+++ NekoDriverIO.cpp
@@ -437,12 +437,16 @@ void UpdateKeypadRegisters()
     //       when port0[3:0] defined as input, it got "on" function and is pulled high.
     if(nc2000mode) {
         if(port1control==0xff&&port0control==0xc0 &&w09_port1_OL==0x00){
-            port0data |=0x03;
+            bool hot_key_pressed=false;
             // note: port0control==0xc0 ----->rw0f_b4_DIR00 ==0x00 && rw0f_b5_DIR01 ==0x00
             for(int y=0;y<8;y++){ 
                 if(keypadmatrix[y][1]){  // if any of this row is pressed, the pull high value get cleared
-                    port0data&=~0x02;
+                    hot_key_pressed=true; 
                 }
+            }
+            if(hot_key_pressed){
+                port0data |=0x03;
+                port0data&=~0x02;
             }
         }
     }
